22073a2f495dc788ddae7a91e146ce8f9aa59233
headneZzz <mr.darkilya@mail.ru>
2019-11-08 18:17:22 +0300

    Реализована функция отправки файлов

--- src/main/java/ru/gosarcho/digitqueryspring/controller/MainController.java
+++ src/main/java/ru/gosarcho/digitqueryspring/controller/MainController.java
@@ -1,8 +1,13 @@
 package ru.gosarcho.digitqueryspring.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +21,16 @@ import org.springframework.ui.Model;
 import ru.gosarcho.digitqueryspring.model.PersonModel;
 import ru.gosarcho.digitqueryspring.service.AffairService;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 @Controller
 public class MainController {
 
-    private static List<AffairModel> affairModelList = new ArrayList<>();
+    private static List<AffairModel> affairModels = new ArrayList<>();
+    private static List<File> affairFiles = new ArrayList<>();
     private static PersonModel person;
+    private final String AFFAIRS_FOLDER_DIRECTORY = "D:\\test\\";
+    private final String READING_ROOM_DIRECTORY = "D:\\test\\";
     @Value("${error.message}")
     private String errorMessage;
 
@@ -53,8 +63,8 @@ public class MainController {
     @GetMapping("/affairsList")
     public String affairList(Model model) {
         model.addAttribute("person", person);
-        model.addAttribute("affairs", affairModelList);
-        if (affairModelList.size() != 0) {
+        model.addAttribute("affairs", affairModels);
+        if (affairModels.size() != 0) {
             model.addAttribute("isSending", true);
         }
         return "affairsList";
@@ -62,17 +72,30 @@ public class MainController {
 
     @PostMapping("/affairsList")
     public String saveAndLoadAffairList() {
-        for (AffairModel affairObj : affairModelList) {
+        for (File affair : affairFiles) {
+            try {
+                String[] cutName = affair.getName().split("_");
+                File out = new File(READING_ROOM_DIRECTORY + person.getReaderFullName()
+                        + File.separator + cutName[0] + "_" + cutName[1] + "_" + cutName[2]);
+                out.mkdirs();
+                Files.copy(affair.toPath(), new File(out.toString() + File.separator + affair.getName()).toPath(),
+                        REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (AffairModel affairModel : affairModels) {
             Affair affair = new Affair();
-            affair.setFond(affairObj.getFond());
-            affair.setOp(affairObj.getOp());
-            affair.setAffair(affairObj.getAffair());
+            affair.setFond(affairModel.getFond());
+            affair.setOp(affairModel.getOp());
+            affair.setAffair(affairModel.getAffair());
             affair.setPerson(person.getReaderFullName());
             affair.setExecutor(person.getExecutorLastName());
             affair.setReceiptDate(LocalDate.now());
             affairService.save(affair);
         }
-        affairModelList.clear();
+        affairModels.clear();
         return "load";
     }
 
@@ -84,7 +107,6 @@ public class MainController {
 
     @PostMapping("/addAffair")
     public String saveAffair(Model model, @ModelAttribute("affairForm") AffairForm affairForm) {
-
         String fond = affairForm.getFond();
         String op = affairForm.getOp();
         String affair = affairForm.getAffair();
@@ -92,14 +114,20 @@ public class MainController {
         if (fond != null && fond.length() > 0
                 && op != null && op.length() > 0
                 && affair != null && affair.length() > 0) {
-            affairModelList.add(new AffairModel(fond, op, affair));
 
-            return "redirect:/affairsList";
+            File affairDir = new File(AFFAIRS_FOLDER_DIRECTORY + fond + File.separator
+                    + op + File.separator
+                    + affair + File.separator);
+            if (affairDir.exists()) {
+                affairModels.add(new AffairModel(fond, op, affair));
+                affairFiles.addAll(Arrays.asList(Objects.requireNonNull(affairDir.listFiles())));
+                return "redirect:/affairsList";
+            }
+            model.addAttribute("errorMessage", errorMessage);
+            return "addAffairs";
         }
 
         model.addAttribute("errorMessage", errorMessage);
         return "addAffairs";
     }
-
 }
-
